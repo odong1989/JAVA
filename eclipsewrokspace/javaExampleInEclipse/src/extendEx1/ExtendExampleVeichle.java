76bda05f53cc922eccb21b09c4c8552290ca3b27
김보성 <kwunodong@gmail.com>
2019-11-14 20:55:05 +0900

    mac에서 업로드

---
+++ eclipsewrokspace/javaExampleInEclipse/src/extendEx1/ExtendExampleVeichle.java
@@ -0,0 +1,17 @@
+package extendEx1;
+
+public class ExtendExampleVeichle {
+	//변수 
+	int numberOfWheel;		//바퀴의 갯수 
+	int numberOfSeat;		//좌석의 수 
+	double weight;			//차량의 무게
+		
+	//메소드 
+	public void start() {
+		System.out.println("차 출발함.");
+		}	//차량을 출발시킴
+	public void stop() {
+		System.out.println("차 멈춤");	
+	}	//차량을 정지시킴
+	
+}
