0bd269f857e9aff5640a92ca4aaee2bcec3ea9f5
odong1989 <kwunodong@gmail.com>
2019-11-20 21:17:32 +0900

    SES 노트북 업뎃 이클립스 경로 조정 중 - 삭제된 내역으로 추정.

--- eclipsewrokspace/memberboard191114/src/global/sesoc/oop/service/BoardManagerImplements.java
+++
@@ -1,7 +0,0 @@
-package global.sesoc.oop.service;
-//package END--------------------------------------------------------
-//import END--------------------------------------------------------
-
-public class BoardManagerImplements implements BoardManager {
-
-}
